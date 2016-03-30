c3586e462b7939ce7913d0e0133888289383e553
tbossard <tbossard@adm-zz.mfr-mb.local>
2016-03-30 17:22:04 +0200

    initial

---
+++ src/main/java/fr/istic/dudcl/tp_maven/App.java
@@ -0,0 +1,15 @@
+package fr.istic.dudcl.tp_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
+
---
+++ src/main/java/fr/istic/dudcl/tp_maven/zz.java
@@ -0,0 +1,11 @@
+package fr.istic.dudcl.tp_maven;
+
+	public class zz {
+		public int var2;
+		public int var3;
+		
+		public void testvar() {
+		var2 = 2;
+		}
+		}
+
